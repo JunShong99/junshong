0c382eb56f02ae36e63eb302735c10a59933b7db
JunShong99 <60839609+JunShong99@users.noreply.github.com>
2020-04-05 21:13:49 +0800

    Add files via upload

---
+++ delete later/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("enter what you want to delete");
+	// write your code here
+    }
+}
